e886e0d311c41e4e3cb6fa47c050b4b956f47c34
Cherry Luo <csu20140909@gmail.com>
2019-09-01 15:59:31 +0800

    added layer customization

--- tf_regression_demo.py
+++ tf_regression_demo.py
@@ -102,20 +102,16 @@ def plot_history(history):
   plt.figure()
   plt.xlabel('Epoch')
   plt.ylabel('Mean Abs Error [MPG]')
-  plt.plot(hist['epoch'], hist['mean_absolute_error'],
-           label='Train Error')
-  plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
-           label = 'Val Error')
+  plt.plot(hist['epoch'], hist['mean_absolute_error'], label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label = 'Val Error')
   plt.ylim([0,5])
   plt.legend()
 
   plt.figure()
   plt.xlabel('Epoch')
   plt.ylabel('Mean Square Error [$MPG^2$]')
-  plt.plot(hist['epoch'], hist['mean_squared_error'],
-           label='Train Error')
-  plt.plot(hist['epoch'], hist['val_mean_squared_error'],
-           label = 'Val Error')
+  plt.plot(hist['epoch'], hist['mean_squared_error'], label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mean_squared_error'], label = 'Val Error')
   plt.ylim([0,20])
   plt.legend()
   plt.show()
